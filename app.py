2f1213e3201b93fa3f4b8f6e567a078b8840ca4e
Cecilia Barnard <barnar3@clemson.edu>
2025-11-19 12:22:23 -0500

    got basic table to run on Flask

--- app.py
+++ app.py
@@ -1,29 +1,25 @@
-from flask import Flask, render_template, jsonify
-import csv
-import os 
-import _sqlite3
+
+from flask import Flask
+import sqlite3
 
 app = Flask(__name__)
 
-from flask import current_app, g
+@app.route('/saints')
+def show_saints():
+    conn = sqlite3.connect('scotsirishsaints.db')
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM saints')  # Change 'saints' to your actual table name
+    rows = cur.fetchall()
+    html = '<table border="1">'
+    html += '<tr>' + ''.join(f'<th>{col}</th>' for col in rows[0].keys()) + '</tr>' if rows else ''
+    for row in rows:
+        html += '<tr>' + ''.join(f'<td>{row[col]}</td>' for col in row.keys()) + '</tr>'
+    html += '</table>'
+    return html if rows else 'No data found.'
 
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5001)
 
-def get_db():
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['scotsirishsaints.db'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
-
-    return g.db
-
-
-def close_db(e=None):
-    db = g.pop('db', None)
-
-    if db is not None:
-        db.close()
-        
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
