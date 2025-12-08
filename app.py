1587c1f232e4c8c75f7759309472129fd5db12ab
Cecilia Barnard <barnar3@clemson.edu>
2025-12-08 13:58:04 -0500

    deleted all unnecessary files

--- app.py
+++ app.py
@@ -1,39 +1,10 @@
 
+
 from flask import Flask, render_template
 import plotly.graph_objects as go
 import pandas as pd
 
 app = Flask(__name__)
-
-@app.route('/birth_region_choropleth')
-def birth_region_choropleth():
-    import plotly.express as px
-    import json
-    df = pd.read_csv('SaintBirths_geocoded.csv')
-    # Count births per region
-    birth_counts = df['Birth Region'].value_counts().reset_index()
-    birth_counts.columns = ['region', 'count']
-    # Merge coordinates
-    coords = df[['Birth Region', 'Birth Region Latitude', 'Birth Region Longitude']].drop_duplicates()
-    birth_counts = birth_counts.merge(coords, left_on='region', right_on='Birth Region', how='left')
-    # Load the combined GeoJSON file
-    with open('geojson/combined_british_isles.geojson', 'r') as f:
-        geojson = json.load(f)
-    fig = px.choropleth_mapbox(
-        birth_counts,
-        geojson=geojson,
-        locations='region',
-        color='count',
-        featureidkey='properties.name',
-        mapbox_style='open-street-map',
-        center={'lat': 53.5, 'lon': -7.5},
-        zoom=5,
-        title='Saints Births by Region'
-    )
-    birth_region_choropleth_html = fig.to_html(full_html=False)
-    return render_template('birth_region_choropleth.html', birth_region_choropleth_html=birth_region_choropleth_html)
-
-
 
 @app.route('/monasteries')
 def monasteries():
